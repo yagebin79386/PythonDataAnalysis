146644a362d5963730d2266424d627ea7f8257a7
Ruiqi Tan <156700468+yagebin79386@users.noreply.github.com>
2025-02-17 14:22:11 +0100

    Create My_queries.sql

---
+++ My_queries.sql
@@ -0,0 +1,33 @@
+--CREATE TABLE users(
+--    username TEXT, 
+--    name, 
+--    lastname,
+--    email
+--    )
+
+--INSERT INTO users(username, name,lastname,email)
+--VALUES("vanessa24","vanessa","aldaham","vanessa@yahoo.com");
+
+--INSERT INTO users(username, name,lastname,email)
+--VALUES("mirco","mirco","musk","mirco@yahoo.com");
+
+--INSERT INTO users(username, name,lastname,email)
+--VALUES("hind24","hind","queen","hind@yahoo.com");
+
+--INSERT INTO users(username, name,lastname,email)
+--VALUES("nick24","nick","geek","nick@yahoo.com");
+
+--INSERT INTO users(username, name,lastname,email)
+--VALUES("maria","maria","miraculous","maria@yahoo.com");
+
+--INSERT INTO users(username, name,lastname,email)
+--VALUES("marco","","amazon","marco@yahoo.com");
+
+--INSERT INTO users(username, name,lastname,email)
+--VALUES("Hacker","","beautiful","");
+
+--INSERT INTO users(username, name,lastname,email)
+--VALUES("mirco","mirco","musk","mirco@yahoo.com");
+
+INSERT INTO users(username, name,lastname,email)
+VALUES("Hamza","hamza25","Microsoft","hamza@hotmail.com");
